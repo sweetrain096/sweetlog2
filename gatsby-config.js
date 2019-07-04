6367d96291ad1cea5c171fb0bc756dd4f084a233
sweetrain096 <gpfhddl09@gmail.com>
2019-07-04 10:36:25 +0900

    change color of mobile title bar

--- gatsby-config.js
+++ gatsby-config.js
@@ -175,7 +175,7 @@ module.exports = {
         short_name: siteConfig.title,
         start_url: '/',
         background_color: '#FFF',
-        theme_color: '#F7A046',
+        theme_color: '#FFC1CC',
         display: 'standalone',
         icon: 'static/photo.jpg'
       },
