898750d2c3e9161ac2819adff09aedbb4c165c50
sweetrina096 <gpfhddl09@gmail.com>
2020-01-11 01:24:43 +0900

    [#55] modify add-txt.js

--- src/templates/add-txt.js
+++ src/templates/add-txt.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
-export default () => {
+const AddTxt = () => {
     return (
         <Layout>
             <pre>google.com, pub-4161990653902092, DIRECT, f08c47fec0942fa0</pre>
         </Layout>
-    )
-}
+    );
+};
+
+export default AddTxt;
