4781f744e8cf9e2431ab1f881fb34c0680481331
sweetrina096 <gpfhddl09@gmail.com>
2020-06-08 17:26:33 +0900

    [#75] search console 수정

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -15,7 +15,9 @@ const Layout = ({ children, title, description }: Props) => (
     <Helmet>
       <html lang="en" />
       <title>{title}</title>
-      <meta name="google-site-verification" content="tqJ5CTRVzlxtY-rn0ACl7LdGIaXvFYoIIf6P61K3_J0" />
+      {/* <meta name="google-site-verification" content="tqJ5CTRVzlxtY-rn0ACl7LdGIaXvFYoIIf6P61K3_J0" /> */}
+      {/* 변경사항 */}
+      <meta name="google-site-verification" content="au8g6fNCN4vLVwHlvyiojCLD85xNQOOO7ZMaSHNtolw" />
       <meta name="description" content={description} />
       <meta property="og:site_name" content={title} />
       <meta name="twitter:card" content="summary" />
